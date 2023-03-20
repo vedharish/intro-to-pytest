fd196c6d551a4f8d91822aa7e615c7bbbb482f22
Harish Ved <ved.harish3@gmail.com>
2023-03-20 16:43:15 +0530

    adding org

---
+++ testselection/org.py
@@ -0,0 +1,9 @@
+from testselection.group import Group
+
+class Org():
+    fail_all = False
+
+    def __init__():
+        self.group = Group()
+        self.should_fail = Org.fail_all or self.group.should_fail
+
