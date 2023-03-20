ff07c7eab5b4ba0ee4e554ebaad9209d12f54631
Harish Ved <ved.harish3@gmail.com>
2023-03-20 16:47:01 +0530

    Adding test cases

--- testselection/tests/test_org.py
+++ testselection/tests/test_org.py
@@ -10,3 +10,15 @@ class TestUser():
         Helper.add_sleep(1)
         org = Org(200)
         assert 200 == org.get_balance()
+
+    def test_deposit(mocker):
+        Helper.add_sleep(1)
+        org = Org(200)
+        org.deposit_money(100)
+        assert 300 == org.get_balance()
+
+    def test_withdraw(mocker):
+        Helper.add_sleep(1)
+        org = Org(200)
+        org.withdraw_money(100)
+        assert 100 == org.get_balance()
