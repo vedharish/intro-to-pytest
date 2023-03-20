79f4940f0a8155c8d355f0fc89a3f188ff647ed0
Harish Ved <ved.harish3@gmail.com>
2023-03-20 15:50:49 +0530

    Adding probability

--- testselection/group.py
+++ testselection/group.py
@@ -5,7 +5,15 @@ class Group():
 
     def __init__(self):
         self.user = User()
+        self.should_fail = Group.fail_all or self.user.fail_all
+
+    def fail_with_probability(self, probability):
+        return random.random() < probability
 
     def return_true(self):
-        print(self.user.fail_all)
         return True
+
+    def random_functionality(self):
+        if self.should_fail:
+            return 999
+        return 999 if self.fail_with_probability(0.2) else 1
