f2066fcaf7704291c0984e06824d93643d275eb6
Harish Ved <ved.harish3@gmail.com>
2023-03-20 14:56:27 +0530

    Adding test cases for functionality

--- testselection/tests/test_user.py
+++ testselection/tests/test_user.py
@@ -15,3 +15,13 @@ class TestUser():
         Helper.add_sleep(0.8)
         user = User()
         assert 9 == user.add_two_numbers(4, 5)
+
+    def test_addition_odd(mocker):
+        Helper.add_sleep(0.2)
+        user = User()
+        assert 4 == user.add_two_numbers(1, 3)
+
+    def test_addition_even(mocker):
+        Helper.add_sleep(1.2)
+        user = User()
+        assert 6 == user.add_two_numbers(2, 4)
