a1e7641ed1086a09023601963c9cf710f497888b
Harish Ved <ved.harish3@gmail.com>
2023-03-20 16:48:24 +0530

    Adding cases

--- testselection/tests/test_org.py
+++ testselection/tests/test_org.py
@@ -12,13 +12,34 @@ class TestUser():
         assert 200 == org.get_balance()
 
     def test_deposit(mocker):
-        Helper.add_sleep(1)
+        Helper.add_sleep(0.4)
         org = Org(200)
         org.deposit_money(100)
         assert 300 == org.get_balance()
 
     def test_withdraw(mocker):
-        Helper.add_sleep(1)
+        Helper.add_sleep(0.6)
         org = Org(200)
         org.withdraw_money(100)
         assert 100 == org.get_balance()
+
+    def test_custom_1(mocker):
+        Helper.add_sleep(1.2)
+        org = Org(200)
+        org.deposit_money(100)
+        org.withdraw_money(100)
+        assert 200 == org.get_balance()
+
+    def test_custom_2(mocker):
+        Helper.add_sleep(2.2)
+        org = Org(200)
+        org.withdraw_money(100)
+        org.deposit_money(100)
+        assert 200 == org.get_balance()
+
+    def test_custom_3(mocker):
+        Helper.add_sleep(0.2)
+        org = Org(200)
+        org.withdraw_money(100)
+        org.withdraw_money(100)
+        assert 0 == org.get_balance()
