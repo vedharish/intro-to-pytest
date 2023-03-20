474f503f86b9c9944ed135db4c74e60667837e4d
Harish Ved <ved.harish3@gmail.com>
2023-03-20 16:44:50 +0530

    Adding balance functionality

--- testselection/org.py
+++ testselection/org.py
@@ -3,7 +3,16 @@ from testselection.group import Group
 class Org():
     fail_all = False
 
-    def __init__():
+    def __init__(self, balance):
         self.group = Group()
         self.should_fail = Org.fail_all or self.group.should_fail
+        self.balance = balance
 
+    def deposit_money(self, amount):
+        self.balance = self.balance + amount
+
+    def withdraw_money(self, amount):
+        self.balance = self.balance - amount
+
+    def get_balance(self):
+        return self.balance
